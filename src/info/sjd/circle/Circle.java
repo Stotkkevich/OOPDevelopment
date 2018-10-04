40509ddf543a88dad29fb375fe1054fb0c718ead
Vladislav Bógdan <bogdan-vlad94@ukr.net>
2018-10-04 14:40:09 +0300

    Add files via upload

--- src/info/sjd/circle/Circle.java
+++ src/info/sjd/circle/Circle.java
@@ -1,10 +1,13 @@
 package info.sjd.circle;
 
+import java.util.logging.Logger;
+
 import info.sjd.AbstractShape;
-import info.sjd.Shape;
 
 public class Circle extends AbstractShape {
 
+	private static Logger logger = Logger.getLogger(Circle.class.getName());
+	
 	private int radius;
 
 	public int getRadius() {
@@ -20,4 +23,9 @@ public class Circle extends AbstractShape {
 		return (int)(Math.PI*radius*radius);
 	}
 
+	public static void main(String[] args) {
+		Circle circle = new Circle();
+		circle.setRadius(128);
+		logger.info("Circle with radius " + circle.getRadius() + " has area " + circle.getArea());
+	}
 }
