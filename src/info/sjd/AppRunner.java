a8326a48720febc4ca3292d27ed6b488e1c87373
Vladislav Bógdan <bogdan-vlad94@ukr.net>
2018-10-09 15:34:14 +0300

    Add files via upload

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -11,17 +11,17 @@ public class AppRunner {
 	private static Logger logger = Logger.getLogger(AppRunner.class.getName());
 	
 	public static void main(String[] args) {
-		Square square = new Square();
-		square.setSide(24);
-		logger.info("Square with site " + square.getSide() + " has area " + square.getArea());
-		
 		Circle circle = new Circle();
-		circle.setRadius(48);
+		circle.setRadius(5);
 		logger.info("Circle with radius " + circle.getRadius() + " has area " + circle.getArea());
 		
+		Square square = new Square();
+		square.setSide(8);
+		logger.info("Square with site " + square.getSide() + " has area " + square.getArea());
+		
 		Triangle triangle = new Triangle();
-		triangle.setBase(64);
-		triangle.setHeight(128);
+		triangle.setBase(10);
+		triangle.setHeight(6);
 		logger.info("Triangle with base " + triangle.getBase() + " and height " + triangle.getHeight() + " has area " + triangle.getArea());
 		
 	}
